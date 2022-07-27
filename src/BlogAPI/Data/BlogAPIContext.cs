16fb627362b0952e9724ed2a654d530604667628
Gilthayllor Brandão <gilthayllor@outlook.com>
2022-07-27 01:30:42 -0300

    Criação do contexto de dados

---
+++ src/BlogAPI/Data/BlogAPIContext.cs
@@ -0,0 +1,26 @@
+﻿using BlogAPI.Data.Mappings;
+using BlogAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlogAPI.Data
+{
+    public class BlogAPIContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=localhost;Database=BlogDatabase;User Id=sa;Password=@sqlserver2022");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new UserMap());
+            modelBuilder.ApplyConfiguration(new PostMap());
+            modelBuilder.ApplyConfiguration(new CategoryMap());
+            modelBuilder.ApplyConfiguration(new TagMap());
+        }
+    }
+}
