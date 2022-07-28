652abffebe4002b9e79608e90ae83a5132c2dc18
Gilthayllor Brandão <gilthayllor@outlook.com>
2022-07-27 22:55:57 -0300

    Add HomeController

--- src/BlogAPI/Program.cs
+++ src/BlogAPI/Program.cs
@@ -1,5 +1,12 @@
+using BlogAPI.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddDbContext<BlogAPIContext>();
+
 var app = builder.Build();
+app.MapControllers();
 
 app.Run();
 
