57f913608b9804c11683565e6acbd12e72c23183
Gilthayllor Brandão <gilthayllor@outlook.com>
2022-07-27 22:58:54 -0300

    Revert "Add HomeController"
    This reverts commit 652abffebe4002b9e79608e90ae83a5132c2dc18.

--- src/BlogAPI/Program.cs
+++ src/BlogAPI/Program.cs
@@ -1,12 +1,5 @@
-using BlogAPI.Data;
-
 var builder = WebApplication.CreateBuilder(args);
-
-builder.Services.AddControllers();
-builder.Services.AddDbContext<BlogAPIContext>();
-
 var app = builder.Build();
-app.MapControllers();
 
 app.Run();
 
