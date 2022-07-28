0bc4f72350856a55ea7bab38cdddea6a3e502418
Gilthayllor Brandão <gilthayllor@outlook.com>
2022-07-27 23:02:28 -0300

    Add controllers and context in services

--- src/BlogAPI/Program.cs
+++ src/BlogAPI/Program.cs
@@ -1,5 +1,11 @@
+using BlogAPI.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddDbContext<BlogAPIContext>();
+
 var app = builder.Build();
+app.MapControllers();
 
 app.Run();
-
