be5f7899b9ae73bcd1c234d1f247a1e648777ca6
Gilthayllor Brandão <gilthayllor@outlook.com>
2022-10-28 05:06:46 -0300

    ModelState extensions methods

---
+++ src/BlogAPI/Extensions/ModelStateExtension.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace BlogAPI.Extensions
+{
+    public static class ModelStateExtension
+    {
+        public static List<string> GetErrors(this ModelStateDictionary modelState)
+        {
+            var errors = new List<string>();
+            
+            foreach(var val in modelState.Values)
+            {
+                errors.AddRange(val.Errors.Select(x => x.ErrorMessage));
+            }
+
+            return errors;  
+        }
+    }
+}
