6534228039fe56150346d04538cde72085e1dda0
Gilthayllor Brandão <gilthayllor@outlook.com>
2022-11-17 15:09:46 -0300

    Classe para criação do token JWT

---
+++ src/BlogAPI/Services/TokenService.cs
@@ -0,0 +1,34 @@
+﻿using BlogAPI.Extensions;
+using BlogAPI.Models;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace BlogAPI.Services
+{
+    public class TokenService
+    {
+        public string GenerateToken(User user)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(Configuration.JwtKey);
+            var claims = user.GetClaims();
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Expires = DateTime.UtcNow.AddHours(8),
+                SigningCredentials = new SigningCredentials
+                (
+                    new SymmetricSecurityKey(key),
+                    SecurityAlgorithms.HmacSha256Signature
+                ),
+                Subject = new System.Security.Claims.ClaimsIdentity(claims)
+            };
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}
