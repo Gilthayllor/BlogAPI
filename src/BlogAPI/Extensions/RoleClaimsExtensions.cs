31f2722641c3c6bd74393b54880c382e80a325f7
Gilthayllor Brandão <gilthayllor@outlook.com>
2022-11-17 15:09:13 -0300

    Classe de extensão para auxiliar na obtenção das roles e claims

---
+++ src/BlogAPI/Extensions/RoleClaimsExtensions.cs
@@ -0,0 +1,22 @@
+﻿using BlogAPI.Models;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Security.Claims;
+
+namespace BlogAPI.Extensions
+{
+    public static class RoleClaimsExtensions
+    {
+        public static IEnumerable<Claim> GetClaims(this User user)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, user.Email),
+            };
+
+            claims.AddRange(user.Roles.Select(x => new Claim(ClaimTypes.Role, x.Name)));
+
+            return claims;
+        }
+    }
+}
